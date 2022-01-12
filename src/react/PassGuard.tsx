809a281ee11ca3505a85cc18ffbdd5892734c2e0
Ruven Salamon <ruven.salamon@gmail.com>
2022-01-12 16:16:43 +0100

    adds pass guard

---
+++ src/react/PassGuard.tsx
@@ -0,0 +1,52 @@
+import { Wallet } from "@project-serum/anchor";
+import { useAnchorWallet, useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { getCredixPassInfo } from "client/api";
+import { useCallback, useEffect, useState } from "react";
+import { CredixPass } from "types/program.types";
+import React from "react";
+
+interface Props {
+	children?: React.ReactNode;
+}
+
+export const PassGuard = (props: Props) => {
+	const wallet = useWallet();
+	const anchorWallet = useAnchorWallet();
+	const connection = useConnection();
+	const [credixPass, setCredixPass] = useState<CredixPass | null>(null);
+
+	const getCredixPass = useCallback(async () => {
+		if (!wallet.connected) {
+			setCredixPass(null);
+			return;
+		}
+
+		if (wallet.connected && wallet.publicKey) {
+			const credixPass = await getCredixPassInfo(
+				wallet.publicKey,
+				connection.connection,
+				anchorWallet as typeof Wallet
+			);
+			setCredixPass(credixPass);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [connection.connection, wallet.connected]);
+
+	useEffect(() => {
+		getCredixPass();
+	}, [getCredixPass]);
+
+	return credixPass?.active ? (
+		<>{props.children}</>
+	) : (
+		<div className="container">
+			<div className="pass-guard-container">
+				<p>{"The Credix Marketplace is now open to accredited investors and fintech lenders."}</p>
+				<br />
+				<p>{"You will be able to interact with the protocol if you’re whitelisted."}</p>
+				<br />
+				{!wallet.connected && <p>{"Please connect your wallet."}</p>}
+			</div>
+		</div>
+	);
+};
