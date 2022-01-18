81e0659605083294e8168e8681ca10a86c6f1f95
Ruven Salamon <ruven.salamon@gmail.com>
2022-01-18 17:51:54 +0100

    adds a different endpoint because we were getting rate limited (#150) (#151)
    * adds a different endpoint because we were getting rate limited
    
    * change mainnet endpoint to serum version
    
    Co-authored-by: Bouwe Ceunen <bouwe.ceunen@gmail.com>

--- src/types/solana.types.ts
+++ src/types/solana.types.ts
@@ -7,7 +7,7 @@ export enum SolanaCluster {
 export enum RPCEndpoint {
 	LOCALNET = "http://127.0.0.1:8899",
 	DEVNET = "https://api.devnet.solana.com",
-	MAINNET = "https://api.mainnet-beta.solana.com",
+	MAINNET = "https://solana-api.projectserum.com/",
 }
 
 export type PdaSeeds = Array<Buffer | Uint8Array>;
