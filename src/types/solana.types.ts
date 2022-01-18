bfa3056cf9a7947c3ddf5e58c2d0e472d3f2c38b
Ruven Salamon <ruven.salamon@gmail.com>
2022-01-18 17:12:32 +0100

    adds a different endpoint because we were getting rate limited

--- src/types/solana.types.ts
+++ src/types/solana.types.ts
@@ -7,7 +7,7 @@ export enum SolanaCluster {
 export enum RPCEndpoint {
 	LOCALNET = "http://127.0.0.1:8899",
 	DEVNET = "https://api.devnet.solana.com",
-	MAINNET = "https://api.mainnet-beta.solana.com",
+	MAINNET = "https://ssc-dao.genesysgo.net/",
 }
 
 export type PdaSeeds = Array<Buffer | Uint8Array>;
